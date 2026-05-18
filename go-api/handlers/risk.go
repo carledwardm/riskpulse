03a68acf06ec420780fd7c918d280b8187bf24de
carledwardm <cmillardv77@gmail.com>
2026-05-18 15:46:49 -0400

    Debug CORS error with go

--- go-api/handlers/risk.go
+++ go-api/handlers/risk.go
@@ -7,6 +7,7 @@ import (
 
 func RiskHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	data := map[string]any{
 		"stats": map[string]any{
