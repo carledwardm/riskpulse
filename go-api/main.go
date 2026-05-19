396d199fd1ba6cd992ad3e22e1f027163e77d939
carledwardm <cmillardv77@gmail.com>
2026-05-19 17:09:13 -0400

    Change main.go to work on hosting platform

--- go-api/main.go
+++ go-api/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"riskpulse/go-api/handlers"
 )
 
 func main() {
 	http.HandleFunc("/risk", handlers.RiskHandler)
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
 }
